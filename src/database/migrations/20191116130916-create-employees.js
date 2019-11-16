b39d35bda266fcb2157e94755de050cb619280bd
vitorrafael <vrsilveira07@gmail.com>
2019-11-16 10:40:21 -0300

    Adds first migration

---
+++ src/database/migrations/20191116130916-create-employees.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+   return queryInterface.createTable('employees', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    monthly_salary: {
+      type: Sequelize.DOUBLE,
+      allowNull: false
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    }
+   });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
