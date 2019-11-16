fbaaed5e9b6f7c566a0907a7e828014df90c6a9c
vitorrafael <vrsilveira07@gmail.com>
2019-11-16 11:08:45 -0300

    Adds Employee GET route

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,8 @@ const EmployeeController = require('./controllers/EmployeeController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    return res.json({ hello: "Hello World"} );
-})
-
+// Employee related routes
+router.get('/employees', EmployeeController.index);
 router.post('/employees', EmployeeController.store);
 
 module.exports = router;
