d49b6adabba21fc5fd7da6721ac9e72b637c6a21
vitorrafael <vrsilveira07@gmail.com>
2019-11-16 10:38:57 -0300

    Adds database config file

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    port: '3308',
+    username: 'root',
+    password: 'root',
+    database: 'salesnode',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
