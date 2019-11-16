9beb86404e20a24823b3cde06551d8b5225ad574
vitorrafael <vrsilveira07@gmail.com>
2019-11-16 11:04:33 -0300

    Adds Employee model to the database connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
 const dbconfig = require('../config/database');
 
+const Employee = require('../models/Employee');
+
 const connection = new Sequelize(dbconfig);
+
+Employee.init(connection);
 
 module.exports = connection;
