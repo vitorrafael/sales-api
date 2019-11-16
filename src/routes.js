8f403a1a8070d9d3ff4308fda8c9d8f8ba859cba
vitorrafael <vrsilveira07@gmail.com>
2019-11-16 11:04:01 -0300

    Implements Employee POST route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const EmployeeController = require('./controllers/EmployeeController');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     return res.json({ hello: "Hello World"} );
 })
+
+router.post('/employees', EmployeeController.store);
 
 module.exports = router;
