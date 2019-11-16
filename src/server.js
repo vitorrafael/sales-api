08f193ddbc3143cdb691ace5df495833449ba5be
vitorrafael <vrsilveira07@gmail.com>
2019-11-16 10:42:06 -0300

    App main file

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = require('./routes')
+
+const app = express();
+
+app.use(express.json())
+app.use(routes);
+
+app.listen(3333, () => {
+    console.log("Listening to port 3333");
+})
