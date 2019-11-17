aaa7c73d21be26c0aaf07d4379f02d8ef772eb3c
vitorrafael <vrsilveira07@gmail.com>
2019-11-17 12:13:32 -0300

    Implements customer creating and searching for a specific one

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -10,4 +10,25 @@ module.exports = {
       return res.status(200).json(customers);
     }
   },
+
+  async getCustomer(req, res) {
+
+    const { id } = req.params;
+
+    const customer = Customer.findByPk(id);
+    if(!customer) {
+      return res.sendStatus(204);
+    } else {
+      return res.status(200).json(customer);
+    }
+  },
+
+  async store(req, res) {
+    
+    const { name, email } = req.body;
+
+    const customer = Customer.create({ name, email});
+
+    return res.status(201).json(customer);
+  },
 }
