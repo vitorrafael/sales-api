d4613386226fb72c47099128039244913c70ceba
vitorrafael <vrsilveira07@gmail.com>
2019-11-17 12:29:16 -0300

    Connects Customer model to the database

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,11 @@ const Sequelize = require('sequelize');
 const dbconfig = require('../config/database');
 
 const Employee = require('../models/Employee');
+const Customer = require('../models/Customer');
 
 const connection = new Sequelize(dbconfig);
 
 Employee.init(connection);
+Customer.init(connection);
 
 module.exports = connection;
