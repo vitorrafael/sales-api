b45a7840324dae68ad8c3e152859c6732acd9837
vitorrafael <vrsilveira07@gmail.com>
2019-11-17 18:56:13 -0300

    Initializes associations

--- src/database/index.js
+++ src/database/index.js
@@ -4,11 +4,17 @@ const dbconfig = require('../config/database');
 const Employee = require('../models/Employee');
 const Customer = require('../models/Customer');
 const Product = require('../models/Product');
+const Sales = require('../models/Sales');
 
 const connection = new Sequelize(dbconfig);
 
 Employee.init(connection);
 Customer.init(connection);
 Product.init(connection);
+Sales.init(connection);
+
+Sales.associate(connection.models);
+Employee.associate(connection.models);
+Product.associate(connection.models);
 
 module.exports = connection;
