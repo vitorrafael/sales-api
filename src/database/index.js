bdb25f7d5c06268f19de06514571c2a70f9ecf6d
vitorrafael <vrsilveira07@gmail.com>
2019-11-17 12:51:37 -0300

    Initializes Product connection with database

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,12 @@ const dbconfig = require('../config/database');
 
 const Employee = require('../models/Employee');
 const Customer = require('../models/Customer');
+const Product = require('.../models/Product');
 
 const connection = new Sequelize(dbconfig);
 
 Employee.init(connection);
 Customer.init(connection);
+Product.init(connection);
 
 module.exports = connection;
